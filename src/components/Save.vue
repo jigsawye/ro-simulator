a7f5bee766e2f5878b466b2f4fd856b3cd967cf0
jigsawye <jigsaw.ye@gmail.com>
2016-12-07 13:15:29 +0800

    Add save UI component

---
+++ src/components/Save.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+    <div class="control">
+      <span class="select">
+        <select>
+          <option>Save 1: test 1</option>
+          <option>Save 2: test 2</option>
+        </select>
+      </span>
+      <button class="button is-info is-outlined">Save</button>
+      <button class="button is-info is-outlined">Load</button>
+      <button class="button is-danger">Delete</button>
+      <button class="button">Genrate Url</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'save',
+  };
+</script>
---
+++ src/components/Stats/Status.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <nav class="level">
+      <div class="level-item has-text-centered">
+        <p class="heading">ATK</p>
+        <p class="title">{{ atk }} + XXX ± XX</p>
+      </div>
+      <div class="level-item has-text-centered">
+        <p class="heading">MATK</p>
+        <p class="title">{{ matk }} + XXX ± XX</p>
+      </div>
+      <div class="level-item has-text-centered">
+        <p class="heading">CRI</p>
+        <p class="title">{{ cri }}</p>
+      </div>
+    </nav>
+    <nav class="level">
+      <div class="level-item has-text-centered">
+        <p class="heading">DEF</p>
+        <p class="title">XXX + {{ def }}</p>
+      </div>
+      <div class="level-item has-text-centered">
+        <p class="heading">MDEF</p>
+        <p class="title">XXX + {{ mdef }}</p>
+      </div>
+      <div class="level-item has-text-centered">
+        <p class="heading">HIT</p>
+        <p class="title">{{ hit }}</p>
+      </div>
+      <div class="level-item has-text-centered">
+        <p class="heading">FLEE</p>
+        <p class="title">{{ flee }} + {{ dodge }}</p>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    name: 'status',
+    computed: mapGetters([
+      'atk', 'matk', 'hit', 'flee', 'dodge', 'cri', 'def', 'mdef',
+    ]),
+  };
+</script>
